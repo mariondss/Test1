8fb66ef723fc06fb1c6c69f6d1caab83e66ff18a
mariondss <26037@student.dorset-college.ie>
2022-11-10 10:28:50 +0000

    start project

---
+++ W1/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Student stud1 = new Student(26037, "Marion", 0.0, 'F');
+            stud1.DisplayDetails();
+            stud1.AddScord(2.0);
+            stud1.DisplayDetails();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ W1/Student.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace W1
+{
+    class Student
+    {
+        int userName;
+        string name;
+        double score;
+        char nationality;
+
+        public Student(int userName, string name, double score, char nationality)
+        {
+            this.userName = userName;
+            this.name = name;
+            this.score = score;
+            this.nationality = nationality;
+        }
+
+        #region propriété
+        public int UserName
+        {
+            get { return this.userName; }
+            set { this.userName = value; }
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set { this.name = value; }
+        }
+
+        public double Score { get;  set; }
+
+        public char Nationality
+        {
+            get { return this.nationality; }
+            set { this.nationality = value; }
+        }
+        #endregion propriété
+
+        public void DisplayDetails()
+        {
+            Console.WriteLine("UserName : " + this.userName + "\nName : " + this.name + "\nScore : " + this.score + "\nNationality : " + this.nationality);
+        }
+
+        public void AddScord (double add)
+        {
+            Score += add;
+        }
+    }
+}
